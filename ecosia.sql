fa67045532baddc9e741c3e683e13b5fbabb52fe
williamsiewe16 <loicosiewe16@gmail.com>
2023-01-03 21:08:28 +0100

    adding dotenv

--- ecosia.sql
+++ ecosia.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `ecosia`
 --
+CREATE DATABASE IF NOT EXISTS ecosia;
+USE ecosia;
 
 -- --------------------------------------------------------
 
