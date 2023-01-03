408ab63642bcdf8e560d80e25bedc2db1e396f47
williamsiewe16 <loicosiewe16@gmail.com>
2023-01-03 20:52:24 +0100

    adding dotenv

--- api.py
+++ api.py
@@ -1,16 +1,20 @@
 from flask import Flask,render_template, request
 import requests
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_USER'] = os.environ["MYSQL_USER"]
+app.config['MYSQL_PASSWORD'] = os.environ["MYSQL_PASSWORD"]
 app.config['MYSQL_DB'] = 'ecosia'
  
+print(app.config)
 mysql = MySQL(app)
-
 
 
 @app.route('/')
@@ -20,7 +24,7 @@ def alert():
   cursor.execute(sql)
   results = cursor.fetchall()
   x = results[0][1]
-  print("Capteur " + x)
+  print("Raspberry " + x)
 
   #return requests.get("http://localhost:3000/api/", str(x)).content.decode("ascii")
 
